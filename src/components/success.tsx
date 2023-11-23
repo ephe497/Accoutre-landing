5d018fa831af7e1b395bd19f4cb868db3833b45c
Ifeoluwa <ephe497@gmail.com>
2023-11-23 14:29:29 +0000

    succes

--- src/components/success.tsx
+++ src/components/success.tsx
@@ -6,7 +6,7 @@ return(
     <div className="bg-[#FDFFFD] w-[80%] md:w-[30%] m-auto rounded-[14px]">
     <div className="py-[60px] text-center ">
         <img src="check.png" className=" w-[30%] m-auto " />    
-        <p className="font-Josefin mt-[40px] text-[18px] md:text-[20px] lg:text-[24px] font-bold ">Email sent successfully</p>
+        <p className="font-Josefin mt-[40px] text-[18px] md:text-[20px] lg:text-[24px] font-bold ">Successful</p>
         <button className="bg-[#4CBE42] font-PlusJakataSan font-[600] m-auto mt-[46px] block text-[11px] text-white py-[12px] px-[28px] rounded-[7px] "
           style={{boxShadow:'4px 4px black'}}
           onClick={toggleModal}>
